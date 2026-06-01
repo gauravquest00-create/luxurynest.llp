a8c83cac58a0d8ba4509dc3a70584f735e151a05
BlueBuck <gauravquest00@gmail.com>
2026-06-01 17:16:39 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,7 @@ function AppContent() {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/new-launches" element={<NewLaunches />} />
-        <Route path="/cooo" element={<Construction />} />  
+        <Route path="/construction" element={<Construction />} />  
 
         
         {/* 404 Route - Catch all unmatched routes */}
